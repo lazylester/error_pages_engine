0382441140849ac80a915077b36dfb6090499767
Les Nightingill <codehacker@comcast.net>
2014-07-25 17:59:04 -0700

    rail4 compatible first commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 ErrorPagesEngine::Engine.routes.draw do
-  match "/404", :to => "errors#not_found"
-  match "/500", :to => "errors#server_error"
-  match "/422", :to => "errors#bad_request"
+  get "/404", :to => "errors#not_found"
+  get "/500", :to => "errors#server_error"
+  get "/422", :to => "errors#bad_request"
 end
--- spec/dummy/config/environments/development.rb
+++ spec/dummy/config/environments/development.rb
@@ -34,4 +34,7 @@ Dummy::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+
+  # rails4 config
+  config.eager_load = false
 end
